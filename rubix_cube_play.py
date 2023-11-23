e361c348dc63e4e91c328dab12f5bd5f1166210e
Tejansh <89126093+tejanshsachdeva@users.noreply.github.com>
2023-11-24 02:20:37 +0530

    ida added

--- rubix_cube_play.py
+++ rubix_cube_play.py
@@ -7,7 +7,7 @@ from all_states import rotate_cube, rotate_face
 from heapq import heappop, heappush
 
 # Input lere hai
-n = int(input("Please enter the value of n : "))
+n = int(input("Please enter the value of n: "))
 
 # Number of possible moves calculated
 numberOfMoves = 0
@@ -18,7 +18,6 @@ if n % 2 == 0:
 else:
     numberOfMoves = ((n - 1) / 2) * 12
     helper = (n - 1) / 2
-
 
 generalMoves = ["R", "L", "T", "D", "F", "B"]
 possibleMoves = []
@@ -37,7 +36,6 @@ for i in range(len(colors)):
     face = [[colors[i] for x in range(n)] for y in range(n)]
     faces.append(face)
 
-
 # Jumble karne ke liye number of moves input lere hai
 scrambleMoves = int(input("How many moves you want for scrambling? "))
 
@@ -49,17 +47,7 @@ scrambledCube = faces
 for move in selectedMoves:
     scrambledCube = apply_move(scrambledCube, move, n)
 
-# print("Initial State : ")
-# print()
-# display_cube(faces)
-
-# final stae ko print karra hun after scrambling
-# print("Final State : ")
-# print()
-# display_cube(scrambledCube)
-
 print(scrambledCube)
-
 
 class PatternDatabase:
     def __init__(self, n, pattern_positions):
@@ -101,6 +89,7 @@ class PatternDatabase:
         return incorrect_count
 
 
+# Calculate the number of incorrect positions
 def heuristic(cube, solved_state):
     distance = 0
     for face1, face2 in zip(cube, solved_state):
@@ -113,7 +102,8 @@ def heuristic(cube, solved_state):
 pattern_positions = [(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2), (2, 0), (2, 1), (2, 2)]
 pattern_db = PatternDatabase(n, pattern_positions)
 
-def solve_cube(initial_state, solved_state, possible_moves, n):
+
+def bfs_solve_cube(initial_state, solved_state, possible_moves, n):
     visited_states = set()
     queue = deque([(initial_state, [])])
     initial_state_tuple = tuple(tuple(row) for face in initial_state for row in face)
@@ -122,24 +112,18 @@ def solve_cube(initial_state, solved_state, possible_moves, n):
     while queue:
         current_state, moves_so_far = queue.popleft()
 
+        print("-----------------------------------")
         print("Current State:")
         display_cube(current_state)
         print("Moves So Far:", moves_so_far)
-        print("-----------------------------------")
 
         if current_state == solved_state:
             return moves_so_far
 
-        # Compute the heuristic value using the pattern database
-        heuristic_value = pattern_db.compute_heuristic(current_state)
-        queue = deque(
-            sorted(queue, key=lambda x: len(x[1]) + heuristic(x[0], solved_state) + heuristic_value)
-        )
-
         for move in possible_moves:
             new_state = apply_move(current_state, move, n)
             new_state_tuple = tuple(tuple(row) for face in new_state for row in face)
-            
+
             if new_state_tuple not in visited_states:
                 visited_states.add(new_state_tuple)
                 new_moves = moves_so_far + [move]
@@ -147,10 +131,50 @@ def solve_cube(initial_state, solved_state, possible_moves, n):
 
     return None
 
+# Function to solve the cube using Iterative Deepening A* (IDA*)
+def ida_star_search(initial_state, solved_state, heuristic_function, max_depth):
+    threshold = heuristic_function(initial_state)
+    while threshold <= max_depth:
+        result = depth_limited_search(initial_state, solved_state, heuristic_function, threshold)
+        if result is not None:
+            return result
+        threshold *= 2  # Increase the threshold more aggressively
+    return None
 
+def depth_limited_search(current_state, solved_state, heuristic_function, depth):
+    if current_state == solved_state:
+        return []
+
+    if depth == 0:
+        return None
+
+    print("-----------------------------------")
+    print("Current State:")
+    display_cube(current_state)
+    print("Depth:", depth)
+
+    for move in possibleMoves:
+        new_state = apply_move(current_state, move, n)
+        print("Move:", move)
+        print("New State:")
+        display_cube(new_state)
+        result = depth_limited_search(new_state, solved_state, heuristic_function, depth - 1)
+        if result is not None:
+            return [move] + result
+
+    return None
+
+# Function to solve the cube using a hybrid approach of BFS and IDA*
+def solve_cube(initial_state, solved_state, possible_moves, n):
+    if n <= 3:
+        print("Using BFS for smaller cube size.")
+        return bfs_solve_cube(initial_state, solved_state, possible_moves, n)
+    else:
+        print("Using IDA* for larger cube size.")
+        return ida_star_search(initial_state, solved_state, pattern_db.compute_heuristic, 10)
 
 solved_state = faces
-solution = solve_cube(scrambledCube, solved_state, possibleMoves, n)
+solution = ida_star_search(scrambledCube, solved_state, pattern_db.compute_heuristic, 10)
 
 if solution:
     print("Solution found!")
