1e841aea5dfa801d0319a53303b8671e8cbd0d35
Ary0405 <aryansethia2003@gmail.com>
2023-11-26 04:21:34 +0530

    hehe

---
+++ rubix_cube_heuristics.py
@@ -0,0 +1,31 @@
+def heuristic(cube, solved_state):
+    distance = 0
+    for face1, face2 in zip(cube, solved_state):
+        for row1, row2 in zip(face1, face2):
+            for color1, color2 in zip(row1, row2):
+                if color1 != color2:
+                    distance += 1
+    return distance
+
+def manhattan_distance(cubie1, cubie2):
+    return sum(abs(coord1 - coord2) for coord1, coord2 in zip(cubie1, cubie2))
+
+def cubie_to_numeric(cubie):
+    color_mapping = {'R': 1, 'G': 2, 'B': 3, 'Y': 4, 'W': 5, 'O': 6}
+    return tuple(color_mapping[color] for color in cubie)
+
+def heuristic2(cube, solved_state):
+    distance = 0
+    for face1, face2 in zip(cube, solved_state):
+        for cubie1, cubie2 in zip(face1, face2):
+            if cubie1 != cubie2:
+                distance += manhattan_distance(cubie_to_numeric(cubie1), cubie_to_numeric(cubie2))
+    return distance
+
+def generate_pattern_positions(n):
+    pattern_positions = []
+    center = n // 2  
+    for i in range(center - 1, center + 2):
+        for j in range(center - 1, center + 2):
+            pattern_positions.append((i, j))
+    return pattern_positions
--- rubix_cube_play.py
+++ rubix_cube_play.py
@@ -2,9 +2,11 @@ import random
 from rubix_cube_move import apply_move
 from rubix_cube_patterndb import PatternDatabase
 from rubix_cube_astar import a_star_solve_cube
+from rubix_cube_heuristics import *
 
 n = int(input("Please enter the value of n : "))
 
+# Calculating number of moves that are possible for a n*n rubix cube
 numberOfMoves = 0
 helper = 0
 if n % 2 == 0:
@@ -14,57 +16,73 @@ else:
     numberOfMoves = ((n - 1) / 2) * 12
     helper = (n - 1) / 2
 
-
+# Finding all moves that are possible for a n*n rubix cube
 generalMoves = ["R", "L", "T", "D", "F", "B"]
 possibleMoves = []
-
 for i in range(len(generalMoves)):
     for j in range(int(helper)):
         possibleMoves.append(generalMoves[i] + str(j) + "C")
         possibleMoves.append(generalMoves[i] + str(j) + "A")
 
+# Creating a solved n*n rubix cube
 colors = ["R", "G", "O", "B", "Y", "W"]
-
 faces = []
 for i in range(len(colors)):
     face = [[colors[i] for x in range(n)] for y in range(n)]
     faces.append(face)
 
-
+# Taking input for number of times we need to scramble the cube
 scrambleMoves = int(input("How many moves you want for scrambling? "))
-
 selectedMoves = random.choices(possibleMoves, k=scrambleMoves)
-
 scrambledCube = faces
+
+# Scrambling the cube
+middle_stages = []
+no_of_middle_stages = 0
+gap_between_stages = 0
+if n < 5 :
+    no_of_middle_stages = 1
+    gap_between_stages = 3
+elif n>5 and n < 7 :
+    no_of_middle_stages = 2
+    gap_between_stages = 2
+elif n > 7  and n < 15:
+    no_of_middle_stages = 3
+    gap_between_stages = 3
+else:
+    no_of_middle_stages = 6
+    gap_between_stages = 3
+
 for move in selectedMoves:
     scrambledCube = apply_move(scrambledCube, move, n)
+    if len(selectedMoves) > 10:
+        if selectedMoves.index(move) % 2 == 0:
+            middle_stages.append(scrambledCube)
+    else :
+        if selectedMoves.index(move) % 2 == 0:
+            middle_stages.append(scrambledCube)
 
-def heuristic(cube, solved_state):
-    distance = 0
-    for face1, face2 in zip(cube, solved_state):
-        for row1, row2 in zip(face1, face2):
-            for color1, color2 in zip(row1, row2):
-                if color1 != color2:
-                    distance += 1
-    return distance
-def generate_pattern_positions(n):
-    pattern_positions = []
-    center = n // 2  # Find the center position for odd-sized cubes
-
-    for i in range(center - 1, center + 2):
-        for j in range(center - 1, center + 2):
-            pattern_positions.append((i, j))
-
-    return pattern_positions
-
+# Initializing pattern database
 pattern_positions = generate_pattern_positions(n)
 pattern_db = PatternDatabase(n, pattern_positions)
 
 solved_state = faces
-solution = a_star_solve_cube(scrambledCube, solved_state, possibleMoves, n, pattern_db, heuristic)
+
+# Solving the cube
+solution = []
+for i in range(len(middle_stages) - 1, -1, -1):
+    if i == len(middle_stages) - 1:
+        solution = a_star_solve_cube(scrambledCube, middle_stages[i], possibleMoves, n, pattern_db, heuristic2)
+    else:
+        temp_solution = a_star_solve_cube(middle_stages[i], middle_stages[i + 1], possibleMoves, n, pattern_db, heuristic2)
+        if temp_solution:
+            solution += temp_solution
+
+# Final solving
+temp_solution = a_star_solve_cube(middle_stages[0], solved_state, possibleMoves, n, pattern_db, heuristic2)
+if temp_solution:
+    solution += temp_solution
 
 if solution:
     print("Solution found!")
     print("Sequence of moves:", solution)
-else:
-    print("No solution found.")
